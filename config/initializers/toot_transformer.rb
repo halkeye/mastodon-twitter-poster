29ab0a963deecbf87999f7820f559ffb33e75af4
Gavin Mogan <git@gavinmogan.com>
2021-04-27 00:06:21 -0700

    Dont let twitter not returning config data break the app

--- config/initializers/toot_transformer.rb
+++ config/initializers/toot_transformer.rb
@@ -1,19 +1,23 @@
+TootTransformer.twitter_short_url_length = 23
+TootTransformer.twitter_short_url_length_https = 23
 if Rails.env.test? || File.split($0).last == "rake"
   Rails.logger.warn { "Using hardcoded values for twitter url length" }
-  TootTransformer.twitter_short_url_length = 23
-  TootTransformer.twitter_short_url_length_https = 23
-else
-  twitter_client = Twitter::REST::Client.new do |config|
-    config.consumer_key = ENV["TWITTER_CLIENT_ID"]
-    config.consumer_secret = ENV["TWITTER_CLIENT_SECRET"]
-  end
+  return
+end
 
+twitter_client = Twitter::REST::Client.new do |config|
+  config.consumer_key = ENV["TWITTER_CLIENT_ID"]
+  config.consumer_secret = ENV["TWITTER_CLIENT_SECRET"]
+end
+
+begin
+  twitter_config = twitter_client.configuration
   begin
-    twitter_config = twitter_client.configuration
     TootTransformer.twitter_short_url_length = twitter_config.short_url_length
     TootTransformer.twitter_short_url_length_https = twitter_config.short_url_length_https
-  rescue Twitter::Error::Forbidden, Twitter::Error::BadRequest
-    Rails.logger.error { "Missing Twitter credentials" }
-    exit 1
+  rescue
   end
+rescue Twitter::Error::Forbidden, Twitter::Error::BadRequest
+  Rails.logger.error { "Missing Twitter credentials" }
+  exit 1
 end
